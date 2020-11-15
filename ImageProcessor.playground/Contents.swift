e16f670070655860cf5d69e18e65ffe9f2bc6d15
VS.MS <vidya0910@gmail.com>
2020-11-15 17:32:19 +0100

    added Filter class

--- ImageProcessor.playground/Contents.swift
+++ ImageProcessor.playground/Contents.swift
@@ -3,29 +3,113 @@
 import UIKit
 
 let image = UIImage(named: "sample")!
+//let rgbaImage = RGBAImage(image: image)!
 
-let rgbaImage = RGBAImage(image: image)!
+//
+//for y in 0 ..< rgbaImage.height {
+//    for x in 0 ..< rgbaImage.width {
+//        let index = y * rgbaImage.width + x
+//        var pixel = rgbaImage.pixels[index]
+//
+////        let redDiff = Int(pixel.red) - 122
+////        if redDiff > 0 {
+////            pixel.red = UInt8(max(0, min(255, 122+redDiff*5)))
+////        }
+//
+//        let existingAlpha = pixel.alpha
+//        let newAlpha = existingAlpha/2
+//        pixel.alpha = newAlpha
+//
+//        rgbaImage.pixels[index] = pixel
+//
+//    }
+//}
 
 
-for y in 0 ..< rgbaImage.height {
-    for x in 0 ..< rgbaImage.width {
-        let index = y * rgbaImage.width + x
-        var pixel = rgbaImage.pixels[index]
+
+
+class Filter {
+    private let rgbaImage: RGBAImage
+
+    init(rgbaImage: RGBAImage) {
+        self.rgbaImage = rgbaImage
+    }
+
+    func applyFiftyPercentageAlphaFilter() -> RGBAImage {
+        return applyFilter(alpha: 0.5, red: 1, green: 1, blue: 1)
+    }
+
+//    func applyFiftyPercentageRedFilter(_ pixel: Pixel) -> Pixel {
+//        rgbaImage
+//        return applyFilter(alpha: pixel.alpha, red: pixel.red/2, green: pixel.green, blue: pixel.blue)
+//    }
+//
+//    func applyFiftyPercentageGreenFilter(_ pixel: Pixel) -> Pixel {
+//        return applyFilter(alpha: pixel.alpha, red: pixel.red, green: pixel.green/2, blue: pixel.blue)
+//    }
+//
+//    func applyFiftyPercentageBlueFilter(_ pixel: Pixel) -> Pixel {
+//        return applyFilter(alpha: pixel.alpha, red: pixel.red, green: pixel.green, blue: pixel.blue/2)
+//    }
+//
+//    func applyFiftyPercentageContrastFilter(_ pixel: Pixel) -> Pixel {
+//        return applyFilter(alpha: pixel.alpha/2, red: pixel.red/2, green: pixel.green/2, blue: pixel.blue/2)
+//    }
+
+    private func applyFilter(alpha: Double, red: Double, green: Double, blue:Double) -> RGBAImage {
         
-//        let redDiff = Int(pixel.red) - 122
-//        if redDiff > 0 {
-//            pixel.red = UInt8(max(0, min(255, 122+redDiff*5)))
-//        }
-        
-        let existingAlpha = pixel.alpha
-        let newAlpha = existingAlpha/2
-        pixel.alpha = newAlpha
-        
-        rgbaImage.pixels[index] = pixel
-
+        for y in 0 ..< rgbaImage.height {
+            for x in 0 ..< rgbaImage.width {
+                let index = y * rgbaImage.width + x
+                var pixel = rgbaImage.pixels[index]
+                pixel.alpha = UInt8(Double(pixel.alpha) * alpha)
+                pixel.red = UInt8(Double(pixel.red) * red)
+                pixel.blue = UInt8(Double(pixel.blue) * blue)
+                pixel.green = UInt8(Double(pixel.green) * green)
+                rgbaImage.pixels[index] = pixel
+            }
+        }
+        return rgbaImage
     }
 }
 
-let newImage = rgbaImage.toUIImage()
+class ImageProcessor {
+    private let image: UIImage
+    
+    init(image: UIImage) {
+        self.image = image
+    }
+    
+    func applyFilter(filterName: String) -> UIImage {
+        let rgbaImage = RGBAImage(image: image)!
+        let filter = Filter(rgbaImage: rgbaImage)
+        let newRGBAImage = filter.applyFiftyPercentageAlphaFilter()
+        return newRGBAImage.toUIImage()!
+        
+//        for y in 0 ..< rgbaImage.height {
+//            for x in 0 ..< rgbaImage.width {
+//                let index = y * rgbaImage.width + x
+//                var pixel = rgbaImage.pixels[index]
+//                if filterName == "Red" {
+//                    pixel.red = pixel.red/2
+//                }
+//                else if filterName == "Blue" {
+//                    pixel.blue = pixel.blue/2
+//                }
+//                else if filterName == "Green" {
+//                    pixel.green = pixel.green/2
+//                }
+//                else if filterName == "Alpha" {
+//                    pixel.alpha = pixel.alpha/2
+//                }
+//                rgbaImage.pixels[index] = pixel
+//            }
+//
+//        }
+//        return rgbaImage.toUIImage()!
+    }
+}
 
+let imageProcessor = ImageProcessor(image: image)
+let newImage = imageProcessor.applyFilter(filterName: "Blue")
 
