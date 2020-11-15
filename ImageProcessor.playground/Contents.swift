a6ef0ce2aaaa03cb4ca2656e410d5716efdd9857
VS.MS <vidya0910@gmail.com>
2020-11-15 17:54:01 +0100

    refactored

--- ImageProcessor.playground/Contents.swift
+++ ImageProcessor.playground/Contents.swift
@@ -2,32 +2,6 @@
 
 import UIKit
 
-let image = UIImage(named: "sample")!
-//let rgbaImage = RGBAImage(image: image)!
-
-//
-//for y in 0 ..< rgbaImage.height {
-//    for x in 0 ..< rgbaImage.width {
-//        let index = y * rgbaImage.width + x
-//        var pixel = rgbaImage.pixels[index]
-//
-////        let redDiff = Int(pixel.red) - 122
-////        if redDiff > 0 {
-////            pixel.red = UInt8(max(0, min(255, 122+redDiff*5)))
-////        }
-//
-//        let existingAlpha = pixel.alpha
-//        let newAlpha = existingAlpha/2
-//        pixel.alpha = newAlpha
-//
-//        rgbaImage.pixels[index] = pixel
-//
-//    }
-//}
-
-
-
-
 class Filter {
     private let rgbaImage: RGBAImage
     
@@ -113,6 +87,7 @@ class ImageProcessor {
     }
 }
 
+let image = UIImage(named: "sample")!
 let imageProcessor = ImageProcessor(image: image)
 let newImage = imageProcessor.applyFilter(filterName: "Contrast").newRGBAImage.toUIImage()
 
