fde5678977c7357dd7b2b3b89447761feb574b5b
VS.MS <vidya0910@gmail.com>
2020-11-15 13:49:33 +0100

    add red amplifier filter

--- ImageProcessor.playground/Contents.swift
+++ ImageProcessor.playground/Contents.swift
@@ -2,5 +2,25 @@
 
 import UIKit
 
-let image = UIImage(named: "sample")
+let image = UIImage(named: "sample")!
+
+let rgbaImage = RGBAImage(image: image)!
+
+
+for y in 0 ..< rgbaImage.height {
+    for x in 0 ..< rgbaImage.width {
+        let index = y * rgbaImage.width + x
+        var pixel = rgbaImage.pixels[index]
+        
+        let redDiff = Int(pixel.red) - 122
+        if redDiff > 0 {
+            pixel.red = UInt8(max(0, min(255, 122+redDiff*5)))
+        }
+        rgbaImage.pixels[index] = pixel
+
+    }
+}
+
+let newImage = rgbaImage.toUIImage()
+
 
